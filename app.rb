0ec5be6f4f7f3d7b1583fbde54eb31f9ab94037b
github username <github email address>
2018-01-09 17:37:34 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,10 +6,13 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
-  get '/piglatinize' do
-
-  end
+  # get '/piglatinize' do
+  #   erb :results_display
+  # end
   post '/piglatinize' do
-    erb :user_input
+    @user_phrase = params["user_phrase"]
+    @new_variable = PigLatinizer.new
+    # erb :user_input
+    erb :piglatinize
   end
 end
